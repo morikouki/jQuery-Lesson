7af4fc65bd12c93de19994eff8585a4d5be28c76
morikouki <akzouksia@icloud.com>
2020-08-10 15:23:31 +0900

    [Start]jQuery-6復習

--- app2.js
+++ app2.js
@@ -1,11 +1,5 @@
 $(function(){
-	$('.box1').slideDown(function(){
-		$('.box1').css({
-		'background-color': '#0000ff',
-		'width': '200px',
-		'height': '100px'
-	}).slideUp();
-	});
-
-	$('.box2').show(2000);
+	$('.box1').mouseover(function(){
+		$('.box1').css('background-color','#0000ff');
+	})
 });
